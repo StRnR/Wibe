543ec52fb710cbfd26e6afeb41cb7663c3f3ca2b
StRnR <arshia.m1320@gmail.com>
2020-01-07 19:07:07 +0330

    signin/up activity/layout 1.0

--- app/src/main/java/com/mahaventures/wibe/ResetPassActivity.java
+++ app/src/main/java/com/mahaventures/wibe/ResetPassActivity.java
@@ -1,6 +1,14 @@
 package com.mahaventures.wibe;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ResetPassActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resetpass);
+    }
 }
--- app/src/main/java/com/mahaventures/wibe/SignUp2Activity.java
+++ app/src/main/java/com/mahaventures/wibe/SignUp2Activity.java
@@ -1,6 +1,14 @@
 package com.mahaventures.wibe;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUp2Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup2);
+    }
 }
