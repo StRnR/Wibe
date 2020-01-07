3fbbfe97aef68828e341dd21d85361d904ae38d5
StRnR <arshia.m1320@gmail.com>
2020-01-07 18:54:43 +0330

    color res

--- app/src/main/java/com/mahaventures/wibe/MainActivity.java
+++ app/src/main/java/com/mahaventures/wibe/MainActivity.java
@@ -1,6 +1,16 @@
 package com.mahaventures.wibe;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
 }
