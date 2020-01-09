f52d385dfa3229bc4ea8c1156ad166a86f2bb41b
Aref Azizian <storm.aref@outlook.com>
2020-01-09 14:56:53 +0330

    password check

--- app/src/main/java/com/mahaventures/wibe/Activities/SignUpActivity.java
+++ app/src/main/java/com/mahaventures/wibe/Activities/SignUpActivity.java
@@ -1,17 +1,86 @@
 package com.mahaventures.wibe.Activities;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.mahaventures.wibe.R;
+import com.mahaventures.wibe.Services.StaticTools;
 
 public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        Button signUpButton = findViewById(R.id.btn_signup);
+        TextView passwordTxt = findViewById(R.id.txt_edit_pass_signup);
+        ProgressBar bar = findViewById(R.id.passProgressBar);
+        TextView weaknessTxt = findViewById(R.id.weaknessTxt);
+        weaknessTxt.setVisibility(View.GONE);
+        bar.setVisibility(View.GONE);
+        bar.setMax(10);
 
+        passwordTxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                weaknessTxt.setVisibility(View.VISIBLE);
+                bar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String pass = s.toString();
+                int score = StaticTools.CalculatePasswordStrength(pass);
+                if (score == 0) {
+                    weaknessTxt.setText("!");
+                    int color = getResources().getColor(R.color.unacceptablePass);
+                    bar.getIndeterminateDrawable().setColorFilter(color, PorterDuff.Mode.SRC_IN);
+                    bar.getProgressDrawable().setColorFilter(color, PorterDuff.Mode.SRC_IN);
+                    weaknessTxt.setTextColor(color);
+                    bar.setProgress(2);
+                } else if (score <= 4) {
+                    weaknessTxt.setText("Weak");
+                    int color = getResources().getColor(R.color.weakPass);
+                    bar.getIndeterminateDrawable().setColorFilter(color, PorterDuff.Mode.SRC_IN);
+                    bar.getProgressDrawable().setColorFilter(color, PorterDuff.Mode.SRC_IN);
+                    weaknessTxt.setTextColor(color);
+                    bar.setProgress(4);
+                } else if (score <= 6) {
+                    weaknessTxt.setText("Good");
+                    int color = getResources().getColor(R.color.goodPass);
+                    bar.getIndeterminateDrawable().setColorFilter(color, PorterDuff.Mode.SRC_IN);
+                    bar.getProgressDrawable().setColorFilter(color, PorterDuff.Mode.SRC_IN);
+                    weaknessTxt.setTextColor(color);
+                    bar.setProgress(6);
+                } else if (score <= 8) {
+                    weaknessTxt.setText("Strong");
+                    int color = getResources().getColor(R.color.strongPass);
+                    bar.getIndeterminateDrawable().setColorFilter(color, PorterDuff.Mode.SRC_IN);
+                    bar.getProgressDrawable().setColorFilter(color, PorterDuff.Mode.SRC_IN);
+                    weaknessTxt.setTextColor(color);
+                    bar.setProgress(8);
+                } else if (score >= 10) {
+                    weaknessTxt.setText("Secure");
+                    int color = getResources().getColor(R.color.securePass);
+                    bar.getIndeterminateDrawable().setColorFilter(color, PorterDuff.Mode.SRC_IN);
+                    bar.getProgressDrawable().setColorFilter(color, PorterDuff.Mode.SRC_IN);
+                    weaknessTxt.setTextColor(color);
+                    bar.setProgress(10);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/mahaventures/wibe/Services/StaticTools.java
+++ app/src/main/java/com/mahaventures/wibe/Services/StaticTools.java
@@ -10,4 +10,29 @@ public class StaticTools {
         toast.setDuration(length);
         toast.show();
     }
+
+    public static int CalculatePasswordStrength(String password) {
+        int iPasswordScore = 0;
+        if (password.length() < 8)
+            return 0;
+        else if (password.length() >= 10)
+            iPasswordScore += 2;
+        else
+            iPasswordScore += 1;
+        if (password.matches("(?=.*[0-9].*[0-9]).*"))
+            iPasswordScore += 2;
+        else if (password.matches("(?=.*[0-9]).*"))
+            iPasswordScore += 1;
+        if (password.matches("(?=.*[a-z]).*"))
+            iPasswordScore += 2;
+        if (password.matches("(?=.*[A-Z].*[A-Z]).*"))
+            iPasswordScore += 2;
+        else if (password.matches("(?=.*[A-Z]).*"))
+            iPasswordScore += 1;
+        if (password.matches("(?=.*[~!@#$%^&*()_-].*[~!@#$%^&*()_-]).*"))
+            iPasswordScore += 2;
+        else if (password.matches("(?=.*[~!@#$%^&*()_-]).*"))
+            iPasswordScore += 1;
+        return iPasswordScore;
+    }
 }
