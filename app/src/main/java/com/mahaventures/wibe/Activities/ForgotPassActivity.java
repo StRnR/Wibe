a4fae7630b480e76ce2c48041a0af6139ab15792
StRnR <arshia.m1320@gmail.com>
2020-01-09 21:54:55 +0330

    ui update

--- app/src/main/java/com/mahaventures/wibe/Activities/ForgotPassActivity.java
+++ app/src/main/java/com/mahaventures/wibe/Activities/ForgotPassActivity.java
@@ -1,6 +1,9 @@
 package com.mahaventures.wibe.Activities;
 
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.TouchDelegate;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -15,8 +18,23 @@ public class ForgotPassActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgotpass);
         Button backBtn = findViewById(R.id.btn_back_forgotpass);
-        Button resetPassBtn = findViewById(R.id.btn_sendresetlink);
+        Button sendResetLinkBtn = findViewById(R.id.btn_sendresetlink);
         EditText emailTxt = findViewById(R.id.txt_edit_email_forgotpass);
+
+        final View parent = (View) backBtn.getParent();
+        parent.post( new Runnable() {
+            public void run() {
+                final Rect rect = new Rect();
+                backBtn.getHitRect(rect);
+                rect.top -= 50;
+                rect.left -= 50;
+                rect.bottom += 50;
+                rect.right += 50;
+                parent.setTouchDelegate( new TouchDelegate( rect , backBtn));
+            }
+        });
+
+        backBtn.setOnClickListener(v -> ForgotPassActivity.super.onBackPressed());
 
     }
 }
