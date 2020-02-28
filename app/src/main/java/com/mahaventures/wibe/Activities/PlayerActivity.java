689c3f566f6c9bc05cd12eafc8b0e2c8a2c37b8b
StRnR <arshia.m1320@gmail.com>
2020-02-29 00:33:01 +0330

    main player v1

--- app/src/main/java/com/mahaventures/wibe/Activities/PlayerActivity.java
+++ app/src/main/java/com/mahaventures/wibe/Activities/PlayerActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.mahaventures.wibe.R;
+
+import org.w3c.dom.Text;
 
 public class PlayerActivity extends AppCompatActivity {
 
@@ -13,7 +17,17 @@ public class PlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+        //TODO: in metadata ha bara har song bayad gerefte she az api joz 2 ta avali ke khodam mizanam
+        TextView srcTxt = findViewById(R.id.txt_playersrc);
+        TextView srcNameTxt = findViewById(R.id.txt_srcname_mainplayer);
+        TextView songTitleTxt = findViewById(R.id.txt_title_mainplayer);
+        TextView artistTxt = findViewById(R.id.txt_artist_mainplayer);
+        TextView songTimeTxt = findViewById(R.id.txt_songtime_mainplayer);
+        TextView songDurationTxt = findViewById(R.id.txt_songduration_mainplayer);
+        ProgressBar songProgressBar = findViewById(R.id.progressbar_mainplayer);
         ImageView artwork = findViewById(R.id.img_cover_mainplayer);
+
+
 
     }
 }
