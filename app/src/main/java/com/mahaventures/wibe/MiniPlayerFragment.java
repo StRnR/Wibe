7a560776634de77b346a58cdf76267cf7f9a7fa0
StRnR <arshia.m1320@gmail.com>
2020-03-05 01:55:15 +0330

    miniplayer

--- app/src/main/java/com/mahaventures/wibe/MiniPlayerFragment.java
+++ app/src/main/java/com/mahaventures/wibe/MiniPlayerFragment.java
@@ -1,18 +1,22 @@
 package com.mahaventures.wibe;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MiniPlayerFragment extends Fragment {
+
+    private Button playBtn;
+    private Button skipBtn;
+    private Button rewindBtn;
 
     public MiniPlayerFragment() {
         // Required empty public constructor
@@ -23,6 +27,20 @@ public class MiniPlayerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mini_player, container, false);
+        View view = inflater.inflate(R.layout.fragment_mini_player, container, false);
+        playBtn = view.findViewById(R.id.btn_play_miniplayer);
+        skipBtn = view.findViewById(R.id.btn_skip_miniplayer);
+        rewindBtn = view.findViewById(R.id.btn_rewind_miniplayer);
+
+
+        //TODO: connect to media player
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return view;
     }
 }
