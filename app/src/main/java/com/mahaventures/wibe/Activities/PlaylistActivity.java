e07de988cdd1ffaab3b485da40517f3d4b9fdbb2
StRnR <arshia.m1320@gmail.com>
2020-03-30 21:23:24 +0430

    ui debug

--- app/src/main/java/com/mahaventures/wibe/Activities/PlaylistActivity.java
+++ app/src/main/java/com/mahaventures/wibe/Activities/PlaylistActivity.java
@@ -81,7 +81,6 @@ public class PlaylistActivity extends AppCompatActivity {
         TextView playlistOwner = findViewById(R.id.txt_owner_playlist);
         TextView description = findViewById(R.id.txt_playlist_description);
 
-        //hi
         final View parent = (View) backBtn.getParent();
         parent.post(() -> {
             final Rect rect = new Rect();
