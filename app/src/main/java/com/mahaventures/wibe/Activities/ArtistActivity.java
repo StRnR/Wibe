8618a61f4d97031840526017a57f7a5830b8cc56
StRnR <arshia.m1320@gmail.com>
2020-04-05 21:43:54 +0430

    artist ui connect

--- app/src/main/java/com/mahaventures/wibe/Activities/ArtistActivity.java
+++ app/src/main/java/com/mahaventures/wibe/Activities/ArtistActivity.java
@@ -1,8 +1,12 @@
 package com.mahaventures.wibe.Activities;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.mahaventures.wibe.Models.NewModels.Artist;
 import com.mahaventures.wibe.R;
@@ -15,14 +19,24 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class ArtistActivity extends AppCompatActivity {
-    //todo text view ha ro inja begir
+    public static TextView artistName;
+    public static ImageView artistArtwork;
+    public static ImageView artistBlurred;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artist);
         String id = getIntent().getStringExtra("id");
-        //todo inja vasl kon be xml
+        artistArtwork = findViewById(R.id.img_artwork_artist_page);
+        artistBlurred = findViewById(R.id.img_blur_artist);
+        artistName = findViewById(R.id.txt_name_artist);
+        Button backBtn = findViewById(R.id.btn_back_artist);
+        TextView songsHeader = findViewById(R.id.txt_songs_header_artist);
+        TextView albumsHeader = findViewById(R.id.txt_albums_header_search);
+        TextView songsShowMore = findViewById(R.id.txt_showmore_songs_artist);
+        RecyclerView songsRv = findViewById(R.id.recycler_songs_artist);
+        RecyclerView albumsRv = findViewById(R.id.recycler_albums_artist);
         getArtistData(id);
     }
 
@@ -32,7 +46,7 @@ public class ArtistActivity extends AppCompatActivity {
         call.enqueue(new Callback<Artist>() {
             @Override
             public void onResponse(Call<Artist> call, Response<Artist> response) {
-                if (response.isSuccessful()){
+                if (response.isSuccessful()) {
                     //todo set parameters
                 }
             }
