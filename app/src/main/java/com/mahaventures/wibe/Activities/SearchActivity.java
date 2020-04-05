9c73425c441a39129098324a8539565afe8b3874
Aref Azizian <storm.aref@outlook.com>
2020-04-05 20:41:58 +0430

    toast added for testing

--- app/src/main/java/com/mahaventures/wibe/Activities/SearchActivity.java
+++ app/src/main/java/com/mahaventures/wibe/Activities/SearchActivity.java
@@ -58,12 +58,8 @@ public class SearchActivity extends AppCompatActivity {
     private Timer timer;
     Button clearTxtBtn;
     EditText searchText;
-    TextView songsHeader;
-    TextView albumsHeader;
-    TextView artistsHeader;
-    TextView songsShowAll;
-    TextView albumsShowAll;
-    TextView artistsShowAll;
+    TextView resCategory;
+    public static List<Track> tracks;
 
     @Override
     public void onBackPressed() {
@@ -90,7 +86,6 @@ public class SearchActivity extends AppCompatActivity {
             return false;
         });
         MiniPlayerFragment.isPrepared = true;
-
     }
 
     @Override
@@ -113,12 +108,7 @@ public class SearchActivity extends AppCompatActivity {
         artistsRecycleView.setLayoutManager(artistsLayoutManager);
         clearTxtBtn = findViewById(R.id.btn_clear_search);
         TextView searchHeader = findViewById(R.id.txt_header_search);
-        songsHeader = findViewById(R.id.txt_songs_header_search);
-        albumsHeader = findViewById(R.id.txt_albums_header_search);
-        artistsHeader = findViewById(R.id.txt_artists_header_search);
-        songsShowAll = findViewById(R.id.txt_showall_songs_search);
-        albumsShowAll = findViewById(R.id.txt_showall_albums_search);
-        artistsShowAll = findViewById(R.id.txt_showall_artists_search);
+        resCategory = findViewById(R.id.txt_songs_header_search);
         tracksRecycleView.setAdapter(null);
         SearchTrackAdapter tmpAdapter = new SearchTrackAdapter(null, SearchActivity.this);
         tracksRecycleView.setAdapter(tmpAdapter);
@@ -166,14 +156,8 @@ public class SearchActivity extends AppCompatActivity {
 
         searchText.setOnKeyListener((v, keyCode, event) -> {
             if (event.getAction() == KeyEvent.ACTION_DOWN) {
-                closeKeyboard();
                 search();
-                songsHeader.setVisibility(View.VISIBLE);
-                albumsHeader.setVisibility(View.VISIBLE);
-                artistsHeader.setVisibility(View.VISIBLE);
-                songsShowAll.setVisibility(View.VISIBLE);
-                albumsShowAll.setVisibility(View.VISIBLE);
-                artistsShowAll.setVisibility(View.VISIBLE);
+                closeKeyboard();
             }
             return false;
         });
@@ -205,6 +189,7 @@ public class SearchActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
+                resCategory.setVisibility(View.VISIBLE);
                 clearTxtBtn.setVisibility(View.VISIBLE);
                 if (searchText.getText().toString().equals("")) {
                     clearTxtBtn.setVisibility(View.INVISIBLE);
@@ -279,9 +264,7 @@ public class SearchActivity extends AppCompatActivity {
                                 tracks = tracks.stream().limit(4).collect(Collectors.toList());
                                 SearchTrackAdapter adapter = new SearchTrackAdapter(tracks, SearchActivity.this);
                                 tracksRecycleView.setAdapter(adapter);
-                                PlayerActivity.queue = new ArrayList<>();
-                                PlayerActivity.queue.clear();
-                                PlayerActivity.queue.addAll(tracks);
+                                tracks = new ArrayList<>(response.body().tracks.data);
                             } catch (Exception e) {
                                 StaticTools.LogErrorMessage(e.getMessage() + " wtf is going on");
                             }
