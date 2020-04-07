379dc8b2f746be16d5d43b56550400a6977fa458
StRnR <arshia.m1320@gmail.com>
2020-04-07 19:58:45 +0430

    my profile layout

---
+++ app/src/main/java/com/mahaventures/wibe/Activities/MyProfileActivity.java
@@ -0,0 +1,16 @@
+package com.mahaventures.wibe.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mahaventures.wibe.R;
+
+public class MyProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+    }
+}
