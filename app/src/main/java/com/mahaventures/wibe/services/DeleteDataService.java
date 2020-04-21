0316b3fc28d4126389ed7240d9543fc07a373e2e
Aref Azizian <storm.aref@outlook.com>
2020-04-21 21:27:14 +0430

    loading fix

--- app/src/main/java/com/mahaventures/wibe/services/DeleteDataService.java
+++ app/src/main/java/com/mahaventures/wibe/services/DeleteDataService.java
@@ -1,8 +1,10 @@
 package com.mahaventures.wibe.services;
 
+import retrofit2.Call;
 import retrofit2.http.DELETE;
+import retrofit2.http.Query;
 
 public interface DeleteDataService {
-//    @DELETE("profile/tracks")
-    // TODO: complete this shit
+    @DELETE("profile/tracks")
+    Call<Void> DeleteFromMySong(@Query("track_id") String track_id);
 }
