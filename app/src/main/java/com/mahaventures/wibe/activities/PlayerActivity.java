369705d25051d54bc243194f12cbf6e9c661e3cd
Aref Azizian <storm.aref@outlook.com>
2020-04-25 23:21:07 +0430

    fix

--- app/src/main/java/com/mahaventures/wibe/activities/PlayerActivity.java
+++ app/src/main/java/com/mahaventures/wibe/activities/PlayerActivity.java
@@ -139,7 +139,7 @@ public class PlayerActivity extends AppCompatActivity implements Playable {
         Button shuffleBtn = findViewById(R.id.btn_shuffle_player);
         Button repeatBtn = findViewById(R.id.btn_repeat_player);
 
-        repeatBtn.setBackgroundResource(repeated ? R.drawable.ic_repeat : R.drawable.ic_repeat_blue);
+        repeatBtn.setBackgroundResource(repeated ? R.drawable.ic_repeat_blue : R.drawable.ic_repeat);
         shuffleBtn.setBackgroundResource(shuffle ? R.drawable.ic_random_blue : R.drawable.ic_random);
 
         final View parent = (View) minimizeBtn.getParent();
