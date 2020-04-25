409c06f1a34610c9aa9f9777af9e458610c17a98
Aref Azizian <storm.aref@outlook.com>
2020-04-25 23:34:31 +0430

    fix

--- app/src/main/java/com/mahaventures/wibe/activities/PlayerActivity.java
+++ app/src/main/java/com/mahaventures/wibe/activities/PlayerActivity.java
@@ -90,7 +90,6 @@ public class PlayerActivity extends AppCompatActivity implements Playable {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        StaticTools.LogTimedMessage("on create fuuuuuuuuuuuuuuuuuuuuuuuuuuck");
         MiniPlayerFragment.isLoaded = false;
         setContentView(R.layout.activity_player);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
